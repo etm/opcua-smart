68764a5c79763894e9749f4c235d9d0ce56dc355
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2018-02-24 09:40:14 +0100

    values

--- contrib/myClient.c
+++ contrib/myClient.c
@@ -82,35 +82,54 @@ int main(void) {
     bReq.nodesToBrowseSize = 1;
     bReq.nodesToBrowse[0].nodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER); /* browse objects folder */
     bReq.nodesToBrowse[0].resultMask = UA_BROWSERESULTMASK_ALL; /* return everything */
+    // bReq.nodesToBrowse[0].referenceTypeId = UA_NODEID_NUMERIC(0,UA_NS0ID_HIERARCHICALREFERENCES); /* return hierarchicals */
     UA_BrowseResponse bResp = UA_Client_Service_browse(client, bReq);
-    printf("%-9s %-16s %-16s %-16s\n", "NAMESPACE", "NODEID", "BROWSE NAME", "DISPLAY NAME");
+    printf("%-9s %-16s %-16s %-16s %-9s %-6s\n", "NAMESPACE", "NODEID", "BROWSE NAME", "DISPLAY NAME", "DATATYPE", "VALUE");
     for (size_t i = 0; i < bResp.resultsSize; ++i) {
         for (size_t j = 0; j < bResp.results[i].referencesSize; ++j) {
             UA_ReferenceDescription *ref = &(bResp.results[i].references[j]);
             if(ref->nodeId.nodeId.identifierType == UA_NODEIDTYPE_NUMERIC) {
-                printf("%-9d %-16d %-16.*s %-16.*s\n", ref->browseName.namespaceIndex,
+                printf("%-9d %-16d %-16.*s %-16.*s", ref->browseName.namespaceIndex,
                        ref->nodeId.nodeId.identifier.numeric, (int)ref->browseName.name.length,
                        ref->browseName.name.data, (int)ref->displayName.text.length,
                        ref->displayName.text.data);
             } else if(ref->nodeId.nodeId.identifierType == UA_NODEIDTYPE_STRING) {
-                printf("%-9d %-16.*s %-16.*s %-16.*s\n", ref->browseName.namespaceIndex,
+                printf("%-9d %-16.*s %-16.*s %-16.*s", ref->browseName.namespaceIndex,
                        (int)ref->nodeId.nodeId.identifier.string.length,
                        ref->nodeId.nodeId.identifier.string.data,
                        (int)ref->browseName.name.length, ref->browseName.name.data,
                        (int)ref->displayName.text.length, ref->displayName.text.data);
             }
-            /* TODO: distinguish further types */
+
+            UA_Variant value; /* Variants can hold scalar values and arrays of any type */
+            UA_Variant_init(&value);
+
+            /* NodeId of the variable holding the current time */
+            const UA_NodeId nodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_SERVER_SERVERSTATUS_CURRENTTIME);
+            retval = UA_Client_readValueAttribute(client, ref->nodeId.nodeId, &value);
+
+            if (retval == UA_STATUSCODE_GOOD && UA_Variant_hasScalarType(&value, &UA_TYPES[UA_TYPES_DATETIME])) {
+              UA_DateTime raw_date = *(UA_DateTime *) value.data;
+              UA_DateTimeStruct dts = UA_DateTime_toStruct(raw_date);
+              printf(" %-9s %u-%u-%u %u:%u:%u.%03u\n", "datetime", dts.day, dts.month, dts.year, dts.hour, dts.min, dts.sec, dts.milliSec);
+            } else if (retval == UA_STATUSCODE_GOOD && UA_Variant_hasScalarType(&value, &UA_TYPES[UA_TYPES_INT32])) {
+              UA_Int32 raw_int = *(UA_Int32 *) value.data;
+              printf(" %-9s %-6d\n", "int32", raw_int);
+            } else {
+              printf("\n");
+            }
         }
     }
     UA_BrowseRequest_deleteMembers(&bReq);
     UA_BrowseResponse_deleteMembers(&bResp);
 
-    /* Same thing, this time using the node iterator... */
-    UA_NodeId *parent = UA_NodeId_new();
-    *parent = UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER);
-    UA_Client_forEachChildNodeCall(client, *parent, nodeIter, (void *) parent);
-    UA_NodeId_delete(parent);
+    // /* Same thing, this time using the node iterator... */
+    // UA_NodeId *parent = UA_NodeId_new();
+    // *parent = UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER);
+    // UA_Client_forEachChildNodeCall(client, *parent, nodeIter, (void *) parent);
+    // UA_NodeId_delete(parent);
 
     UA_Client_delete(client); /* Disconnects the client internally */
+
     return UA_STATUSCODE_GOOD;
 }
--- contrib/myServer.c
+++ contrib/myServer.c
@@ -2,20 +2,39 @@
 #include <open62541.h>
 
 UA_Boolean running = true;
-static void stopHandler(int sig) {
-    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "received ctrl-c");
+void signalHandler(int sig) {
     running = false;
 }
 
-int main(void) {
-    signal(SIGINT, stopHandler);
-    signal(SIGTERM, stopHandler);
+int main(int argc, char** argv)
+{
+    signal(SIGINT, signalHandler); /* catch ctrl-c */
 
+    /* Create a server listening on port 4840 */
     UA_ServerConfig *config = UA_ServerConfig_new_default();
     UA_Server *server = UA_Server_new(config);
 
-    UA_StatusCode retval = UA_Server_run(server, &running);
+    /* Add a variable node */
+    /* 1) Define the node attributes */
+    UA_VariableAttributes attr = UA_VariableAttributes_default;
+    attr.displayName = UA_LOCALIZEDTEXT("en-US", "the answer");
+    UA_Int32 myInteger = 42;
+    UA_Variant_setScalar(&attr.value, &myInteger, &UA_TYPES[UA_TYPES_INT32]);
+
+    /* 2) Define where the node shall be added with which browsename */
+    UA_NodeId newNodeId = UA_NODEID_STRING(1, "the.answer");
+    UA_NodeId parentNodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER);
+    UA_NodeId parentReferenceNodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_ORGANIZES);
+    UA_NodeId variableType = UA_NODEID_NULL; /* take the default variable type */
+    UA_QualifiedName browseName = UA_QUALIFIEDNAME(1, "the answer");
+
+    /* 3) Add the node */
+    UA_Server_addVariableNode(server, newNodeId, parentNodeId, parentReferenceNodeId,
+                              browseName, variableType, attr, NULL, NULL);
+
+    /* Run the server loop */
+    UA_StatusCode status = UA_Server_run(server, &running);
     UA_Server_delete(server);
     UA_ServerConfig_delete(config);
-    return (int)retval;
+    return status;
 }
