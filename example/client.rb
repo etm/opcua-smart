2052596416b9172ff2a1d9b5bed8f9e6f137b5bd
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2019-04-17 01:08:46 +0200

    username,pass only in url

--- example/client.rb
+++ example/client.rb
@@ -1,8 +1,7 @@
 #!/usr/bin/ruby
 require_relative '../lib/opcua/client'
 
-# p OPCUA::Client.new("opc.tcp://localhost:4840",nil,nil)
-client = OPCUA::Client.new("opc.tcp://OpcUaClient:SUNRISE@localhost:4840",nil,nil)
+client = OPCUA::Client.new("opc.tcp://OpcUaClient:SUNRISE@localhost:4840")
 node = client.find(2,"/Channel/ProgramInfo/progName")
 
 p node.value
--- lib/opcua/client.rb
+++ lib/opcua/client.rb
@@ -1,2 +1,15 @@
 require_relative 'client.so'
-require 'daemonite'
+
+module OPCUA
+  class Client
+    alias_method :initialize_base, :initialize
+
+    def initialize(url)
+      if url =~ /(^[^:]+):\/\/([^:]+):([^@]+)@(.*)$/
+        initialize_base("#{$1}://#{$4}",$2,$3)
+      else
+        initialize_base(url,nil,nil)
+      end
+    end
+  end
+end
