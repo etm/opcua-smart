d015bb87ce03a00c7f77be8bb4df0e814fa8d4ed
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2019-04-23 15:21:20 +0200

    fixed client example

--- example/client.rb
+++ example/client.rb
@@ -1,17 +1,17 @@
 #!/usr/bin/ruby
 require_relative '../lib/opcua/client'
 
-### username & pass in url
+### username & pass in url (e.g. siemens)
 # client = OPCUA::Client.new("opc.tcp://OpcUaClient:SUNRISE@localhost:4840")
 
 client = OPCUA::Client.new("opc.tcp://localhost:4840")
+client.publishing_interval = 100 # default 500
 
-node = client.get 2, 874565491
+node = client.get 2, 874565491 # get mode from nodeid
 node.on_change do |value,timestamp|
   p value
 end
 
-client.publishing_interval = 100
 while true
   client.check_subscription
   sleep client.publishing_interval / 1000
