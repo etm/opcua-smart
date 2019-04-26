0e0dde5892eecaee749ccc985eec16fcb691fe42
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2019-04-26 02:37:23 +0200

    debug rem

--- example/client.rb
+++ example/client.rb
@@ -7,8 +7,6 @@ require_relative '../lib/opcua/client'
 client = OPCUA::Client.new("opc.tcp://localhost:4840")
 client.subscription_interval = 100 # default 500
 
-exit
-
 node = client.get 2, 874565491 # get node from nodeid
 node.on_change do |value,timestamp|
   p value
