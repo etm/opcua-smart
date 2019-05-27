49146a47c5db458c39ac07ad88a8cdd0b1c12e6c
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2019-05-27 22:03:06 +0200

    server with startup and shutdown

--- example/server.rb
+++ example/server.rb
@@ -3,65 +3,68 @@ require_relative '../lib/opcua/server'
 #require 'opcua/server'
 
 Daemonite.new do
-  begin
-  server = OPCUA::Server.new
-  server.add_namespace "https://centurio.work/kelch"
+  on startup do
+    @opts['server'] = OPCUA::Server.new
+    @opts['server'].add_namespace "https://centurio.work/kelch"
 
-  mt = server.types.add_object_type(:MeasurementType).tap{ |t|
-    t.add_variable :SollWertX
-    t.add_variable :SollWertY
-    t.add_variable :SollWertZ
-  }
-  tt = server.types.add_object_type(:ToolType).tap{ |t|
-    t.add_variable :SollWertX
-    t.add_variable :SollWertY
-    t.add_variable :SollWertZ
-    t.add_variable :ToolNumber
-    t.add_variable :DuploNumber
-    t.add_variable :testValue1
-    t.add_method :testMethod, test1: OPCUA::TYPES::STRING, test2: OPCUA::TYPES::DATETIME do |node, test1, test2, test4|
-      ns, nid = node.id
-      p nid
-      p test1
-      p test2
-      p test4
-      puts 'me'
-      # do something
-    end
-    t.add_object(:Measurements, server.types.folder).tap{ |u|
-      u.add_object :M, mt, OPCUA::OPTIONAL
+    mt = @opts['server'].types.add_object_type(:MeasurementType).tap{ |t|
+      t.add_variable :SollWertX
+      t.add_variable :SollWertY
+      t.add_variable :SollWertZ
     }
-  }
-  pt = server.types.add_object_type(:PresetterType).tap{ |t|
-    t.add_variable :ManufacturerName
-    t.add_object(:Tools, server.types.folder).tap{ |u|
-      u.add_object :Tool, tt, OPCUA::OPTIONAL
+    tt = @opts['server'].types.add_object_type(:ToolType).tap{ |t|
+      t.add_variable :SollWertX
+      t.add_variable :SollWertY
+      t.add_variable :SollWertZ
+      t.add_variable :ToolNumber
+      t.add_variable :DuploNumber
+      t.add_variable :testValue1
+      t.add_method :testMethod, test1: OPCUA::TYPES::STRING, test2: OPCUA::TYPES::DATETIME do |node, test1, test2, test4|
+        ns, nid = node.id
+        p nid
+        p test1
+        p test2
+        p test4
+        puts 'me'
+        # do something
+      end
+      t.add_object(:Measurements, @opts['server'].types.folder).tap{ |u|
+        u.add_object :M, mt, OPCUA::OPTIONAL
+      }
     }
-  }
+    pt = @opts['server'].types.add_object_type(:PresetterType).tap{ |t|
+      t.add_variable :ManufacturerName
+      t.add_object(:Tools, @opts['server'].types.folder).tap{ |u|
+        u.add_object :Tool, tt, OPCUA::OPTIONAL
+      }
+    }
 
-  tools = server.objects.manifest(:KalimatC34, pt).find(:Tools)
+    tools = @opts['server'].objects.manifest(:KalimatC34, pt).find(:Tools)
 
-  t1 = tools.manifest(:Tool1,tt)
-  t2 = tools.manifest(:Tool2,tt)
-  t3 = tools.manifest(:Tool3,tt)
+    t1 = tools.manifest(:Tool1,tt)
+    t2 = tools.manifest(:Tool2,tt)
+    t3 = tools.manifest(:Tool3,tt)
 
-  tn = t1.find(:ToolNumber)
+    @opts[:tn] = t1.find(:ToolNumber)
 
-  measurments_t1 = t1.find(:Measurements)
-  measurments_t1.manifest(:M1,mt)
-  measurments_t1.manifest(:M2,mt)
+    measurments_t1 = t1.find(:Measurements)
+    measurments_t1.manifest(:M1,mt)
+    measurments_t1.manifest(:M2,mt)
 
   rescue => e
     puts e.message
   end
 
   run do
-    begin
-      GC.start
-      sleep server.run
-      tn.value = Time.now
-    rescue => e
-      puts e.message
-    end
+    GC.start
+    sleep @opts['server'].run
+    @opts[:tn].value = Time.now
+  rescue => e
+    puts e.message
+  end
+
+  on exit do
+    # we could disconnect here, but OPCUA::Server does not have an explicit disconnect
+    puts 'bye.'
   end
 end.loop!
