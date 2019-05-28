57369adc8b1379485c4c45904f8be5a4fcb8e040
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2019-05-29 00:25:36 +0200

    client method support

---
+++ example/client_method.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/ruby
+require_relative '../lib/opcua/client'
+#require 'opcua/client'
+
+### username & pass in url (e.g. siemens)
+# client = OPCUA::Client.new("opc.tcp://OpcUaClient:SUNRISE@localhost:4840")
+
+client = OPCUA::Client.new("opc.tcp://localhost:4840")
+client.debug = false
+client.default_ns = 2
+
+node = client.get '/KalimatC34/Tools/Tool3/testMethod'
+node.call 'abcde', Time.now
+client.disconnect
